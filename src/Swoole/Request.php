445e593aef7e7926030999303a66f471d79aedc5
Damodar Lohani <dlohani48@gmail.com>
2021-02-09 16:54:18 +0545

    fix multiple ip issues on proxy servers

--- src/Swoole/Request.php
+++ src/Swoole/Request.php
@@ -127,7 +127,7 @@ class Request extends UtopiaRequest
      */
     public function getIP(): string
     {
-        return $this->getHeader('x-forwarded-for', $this->getServer('remote_addr', '0.0.0.0'));
+        return explode(',',$this->getHeader('x-forwarded-for', $this->getServer('remote_addr', '0.0.0.0')))[0];
     }
 
     /**
