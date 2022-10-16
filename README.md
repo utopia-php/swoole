9f743ba7e4629ad32280004c8aee09effbbe1762
nooras <anoorasfatima@gmail.com>
2022-10-16 15:52:36 +0530

    Feat Code QL Analysis

--- README.md
+++ README.md
@@ -66,7 +66,7 @@ $http->start();
 
 ## System Requirements
 
-Utopia Framework requires PHP 7.4 or later. We recommend using the latest PHP version whenever possible.
+Utopia Framework requires PHP 8.0 or later. We recommend using the latest PHP version whenever possible.
 
 ## Copyright and license
 
--- src/Swoole/Files.php
+++ src/Swoole/Files.php
@@ -7,7 +7,7 @@ use Exception;
 class Files
 {
     /**
-     * @var array
+     * @var array<string, mixed>
      */
     protected static array $loaded = [];
 
@@ -17,12 +17,12 @@ class Files
     protected static int $count = 0;
 
     /**
-     * @var array
+     * @var array<string, mixed>
      */
     protected static array $mimeTypes = [];
 
     /**
-     * @var array
+     * @var array<string, mixed>
      */
     const EXTENSIONS = [
         'css' => 'text/css',
@@ -57,7 +57,7 @@ class Files
     /**
      * Get MimeType List
      *
-     * @return array
+     * @return array<string, mixed>
      */
     public static function getMimeTypes(): array
     {
@@ -160,7 +160,7 @@ class Files
      *
      * @throws \Exception
      */
-    public static function getFileContents(string $uri): string
+    public static function getFileContents(string $uri): mixed
     {
         if (! array_key_exists($uri, self::$loaded)) {
             throw new Exception('File not found or not loaded: '.$uri);
@@ -177,7 +177,7 @@ class Files
      *
      * @throws \Exception
      */
-    public static function getFileMimeType(string $uri): string
+    public static function getFileMimeType(string $uri): mixed
     {
         if (! array_key_exists($uri, self::$loaded)) {
             throw new Exception('File not found or not loaded: '.$uri);
